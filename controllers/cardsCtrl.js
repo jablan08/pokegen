00474724f77b995daa4984902989371232614770
Arrick <Grapes01@gmail.com>
2019-04-24 09:28:06 -0700

    add user index route

--- controllers/cardsCtrl.js
+++ controllers/cardsCtrl.js
@@ -3,6 +3,6 @@ const router  = express.Router();
 const Card = require("../models/cards");
 const User = require("../models/users");
 
-
+router.get('/')
 
 module.exports = router;
--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -4,5 +4,9 @@ const User = require("../models/users");
 const Card = require("../models/cards");
 
 
+router.get('/', (req, res) => {
+    res.render('users/index.ejs')
+})
+
 
 module.exports = router;
