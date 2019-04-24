9072a95868958676ab245908df201f083556bdbb
Jablan <joshuaablan@gmail.com>
2019-04-24 10:02:21 -0700

    connect index.ejs to cardsCtrl

--- controllers/cardsCtrl.js
+++ controllers/cardsCtrl.js
@@ -3,6 +3,31 @@ const router  = express.Router();
 const Card = require("../models/cards");
 const User = require("../models/users");
 
+// INDEX
+router.get("/", async (req,res)=> {
+    try {
+        const allCards = await Card.find({});
+        res.render("cards/index.ejs");
+    } catch(err) {
+        res.send(err)
+    }
+})
+
+
+// NEW
+
+
+
+
+// SHOW
+
+
+
+// EDIT
+
+
+
+// DELETE
 
 
 module.exports = router;
